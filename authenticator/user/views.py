5e5fccc46c255d2ebda25c10c0fc72e6d986db30
krjprogrammer <137873227+krjprogrammer@users.noreply.github.com>
2024-01-10 12:40:26 +0530

    Update views.py

--- authenticator/user/views.py
+++ authenticator/user/views.py
@@ -4,17 +4,25 @@ from .models import User_details
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import User_detail_serializer
+import hashlib
 def user_login(request):
     return render(request,'index.html')
 
 def Register(request):
     return render(request,'register.html')
+    
+def encrypt_pass(password):
+    hash = hashlib.sha256()
+    hash.update(password.encode('utf-8'))
+    encoded_password = hash.hexdigest()
+    return encoded_password
 
 def Login(request):
     if request.method == 'GET':
         user_name = request.GET.get('username')
         pass_word = request.GET.get('pass')
-        matching_user = User_details.objects.filter(username=user_name, password=pass_word)
+        encoded_pass = encrypt_pass(pass_word)
+        matching_user = User_details.objects.filter(username=user_name, password=encoded_pass)
         if matching_user.exists():
             return HttpResponse('User was Loged in successfully')
         else:
@@ -26,7 +34,8 @@ def Get_Data(request):
         pass_word = request.GET.get('pass')
         e_mail = request.GET.get('email')
         if user_name and pass_word :
-            insert = User_details(username = user_name, password = pass_word, email=e_mail)
+            encoded_pass = encrypt_pass(pass_word)
+            insert = User_details(username = user_name, password = encoded_pass, email=e_mail)
             insert.save()
             return HttpResponse(f'User with username {user_name} was saved sucessfully')
         else:
