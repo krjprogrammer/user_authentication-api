71d4c3ff47334cccae5467cb09869fc300e58e66
krjprogrammer <137873227+krjprogrammer@users.noreply.github.com>
2024-01-17 13:19:54 +0530

    Update admin.py

--- authenticator/user/admin.py
+++ authenticator/user/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import User_details
+
 class user_admin(admin.ModelAdmin):
     list_display = ('username','password')
 admin.site.register(User_details,user_admin)
+
+class supply_admin(admin.ModelAdmin):
+    list_display = ('ADMIN_ID','PRODUCT_DEVELOPMENT','PROCUREMENT','MANUFACTURING','DISTRIBUTION','TRANSPORTATION','SERVICE','DEMAND_GEN','FORCASTING','IT','OTHER','R_STUDY')
+admin.site.register(Supply_chain_data,supply_admin)
+
+class firm_admin(admin.ModelAdmin):
+    list_display = ('admin_ID','simulation','firm1','firm2','firm3','firm4','firm5')
+admin.site.register(firm_data,firm_admin)
 # Register your models here.
