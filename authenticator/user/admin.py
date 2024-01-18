9e16cb5cccfc8203b51805e321866a00ef28a996
krjprogrammer <137873227+krjprogrammer@users.noreply.github.com>
2024-01-18 18:51:14 +0530

    Add files via upload

---
+++ authenticator/user/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import User_details,Supply_chain_data,firm_data,Product_config
+class user_admin(admin.ModelAdmin):
+    list_display = ('username','password')
+admin.site.register(User_details,user_admin)
+
+class supply_admin(admin.ModelAdmin):
+    list_display = ('ADMIN_ID','PRODUCT_DEVELOPMENT','PROCUREMENT','MANUFACTURING','DISTRIBUTION','TRANSPORTATION','SERVICE','DEMAND_GEN','FORCASTING','IT','OTHER','R_STUDY')
+admin.site.register(Supply_chain_data,supply_admin)
+
+class firm_admin(admin.ModelAdmin):
+    list_display = ('admin_ID','simulation','firm1','firm2','firm3','firm4','firm5')
+admin.site.register(firm_data,firm_admin)
+
+class product_admin(admin.ModelAdmin):
+    list_display = ( "admin_ID","simulation_id","category","Alpha","Beta","Bandwidth","Warranty","Packaging","Price")
+admin.site.register(Product_config,product_admin)
+# Register your models here.
